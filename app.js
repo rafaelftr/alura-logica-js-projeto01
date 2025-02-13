c08a21450a466ceee4b125c05241572619abb253
fontoura <rafaelftrs@gmail.com>
2025-02-13 02:10:20 -0300

    Refactor game logic to improve user experience and feedback

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 alert("Boas vindas ao jogo do número secreto!")
 let tentativas = 0
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
 let numeroMaximo = 1000
 let numeroSecreto = parseInt((Math.random() * numeroMaximo) + 1)
 
@@ -8,26 +7,24 @@ function verificar() {
     while (true) {
         tentativas++
         let chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}. Se quiser parar, digite "parar"`)
-        if (chute == "parar") {
+        if (isNaN(chute)) {
+            alert("O jogo acabou.")
             break
         }
         if (chute == numeroSecreto) {
             alert("Você acertou!")
             alert(`O número secreto era ${numeroSecreto}`)
-            palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
-            alert(`Voce acertou em ${tentativas} ${palavraTentativa}`)
+            const plural = tentativas > 1 ? 's' : '';
+            alert(`Voce acertou em ${tentativas} tentativa${plural}!`);
             if (confirm('Vamos jogar novamente?')) {
                 numeroSecreto = parseInt((Math.random() * numeroMaximo) + 1)
                 tentativas = 0
             } else {
+                alert('Obrigado por jogar!')
                 break
             }
         } else {
-            if (chute > numeroSecreto) {
-                alert("O número secreto é menor")
-            } else {
-                alert("O número secreto é maior")
-            }
+            alert(`O número secreto é ${chute > numeroSecreto ? 'menor' : 'maior'}`);
         }
     }
 }
